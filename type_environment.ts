7a94b75cd91b32339e388310a3e09ef509377ec4
Christopher Diggins <cdiggins@gmail.com>
2017-12-15 11:21:17 -0500

    deleting old files

--- type_environment.ts
+++
@@ -1,9 +0,0 @@
-import * as ti from "./type_core";
-import * as th from "./type_inference";
-
-// A Type Inference Algorithm 
-// A novel type inference algorithm (not Algorithm W) with support for higher rank polymorphism.
-// Copyright 2017 by Christopher Diggins 
-// Licensed under the MIT License
-
-}
